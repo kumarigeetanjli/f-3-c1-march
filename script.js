ae1fe1cd02013e58aa05b84a7996ee10c64a7d1c
kumarigeetanjli <84951879+kumarigeetanjli@users.noreply.github.com>
2023-03-11 16:13:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,76 +1,91 @@
-function OpeningCeremony(score, callbackFnc) {
+
+//OpeningCeremony() starts here
+
+function OpeningCeremony(point, callbackFnc) {
     setTimeout(() => {
-      console.log("Let the games begin");
-      Race100M(score, callbackFnc);
+        console.log("Lets start the game");
+        Race100M(point, callbackFnc);
     }, 1000);
-  }
-  
-  function Race100M(score, callbackFnc) {
+}
+
+//Race100M() starts here
+
+
+function Race100M(point, callbackFnc) {
     console.log("Race 100M starts now");
     const redTime = Math.floor(Math.random() * 6) + 10;
     const yellowTime = Math.floor(Math.random() * 6) + 10;
     const blueTime = Math.floor(Math.random() * 6) + 10;
     const greenTime = Math.floor(Math.random() * 6) + 10;
-  
+
     let times = {
-      red: redTime,
-      yellow: yellowTime,
-      blue: blueTime,
-      green: greenTime,
+        red: redTime,
+        yellow: yellowTime,
+        blue: blueTime,
+        green: greenTime,
     };
-  
+
     let winners = Object.keys(times).sort((a, b) => times[a] - times[b]);
-  
-    score[winners[0]] += 50;
-    score[winners[1]] += 25;
-  
+
+    point[winners[0]] += 50;
+    point[winners[1]] += 25;
+
     console.log(`Race 100M winner is ${winners[0]} with ${times[winners[0]]} seconds`);
-    console.log(`New Scores: ${JSON.stringify(score)}`);
-  
-    callbackFnc(score, LongJump);
-  }
-  
-  function LongJump(score, callbackFnc) {
+    console.log(`New points: ${JSON.stringify(point)}`);
+
+    callbackFnc(point, LongJump);
+}
+
+//LongJump() starts here
+
+
+function LongJump(point, callbackFnc) {
     console.log("Long Jump starts now");
     const colors = ["red", "yellow", "blue", "green"];
     const chosenColor = colors[Math.floor(Math.random() * 4)];
-    score[chosenColor] += 150;
-  
+    point[chosenColor] += 150;
+
     console.log(`${chosenColor} wins Long Jump`);
-    console.log(`New Scores: ${JSON.stringify(score)}`);
-  
-    callbackFnc(score, HighJump);
-  }
-  
-  function HighJump(score) {
+    console.log(`New points: ${JSON.stringify(point)}`);
+
+    callbackFnc(point, HighJump);
+}
+
+////HighJump() starts here
+
+
+function HighJump(point) {
     console.log("High Jump starts now");
-  
-    const answer = prompt("What colour secured the highest jump?");
+
+    const result = prompt("What colour secured the highest jump?");
     let color;
-    if (answer) {
-      color = answer.toLowerCase();
+    if (result) {
+        color = result.toLowerCase();
     }
-  
+
     if (color === "red" || color === "yellow" || color === "blue" || color === "green") {
-      score[color] += 100;
-    } else if (answer === null) {
-      console.log("Event was cancelled");
+        point[color] += 100;
+    } else if (result === null) {
+        console.log("Event was cancelled");
     } else {
-      console.log("Invalid color entered, no points awarded");
+        console.log("Invalid color entered, no points awarded");
     }
-  
-    console.log(`New Scores: ${JSON.stringify(score)}`);
-    AwardCeremony(score);
-  }
-  
-  function AwardCeremony(score) {
+
+    console.log(`New points: ${JSON.stringify(point)}`);
+    AwardCeremony(point);
+}
+
+//AwardCeremony() starts here
+
+function AwardCeremony(point) {
+
     console.log(`Award Ceremony`);
-    let sortedScores = Object.keys(score).sort((a, b) => score[b] - score[a]);
-  
-    console.log(`1st Place: ${sortedScores[0]} with ${score[sortedScores[0]]} points`);
-    console.log(`2nd Place: ${sortedScores[1]} with ${score[sortedScores[1]]} points`);
-    console.log(`3rd Place: ${sortedScores[2]} with ${score[sortedScores[2]]} points`);
-  }
-  
-  OpeningCeremony({ red: 0, blue: 0, green: 0, yellow: 0 }, Race100M);
-  
+    
+    let sortedpoints = Object.keys(point).sort((a, b) => point[b] - point[a]);
+
+    console.log(`1st Place: ${sortedpoints[0]} with ${point[sortedpoints[0]]} points`);
+    console.log(`2nd Place: ${sortedpoints[1]} with ${point[sortedpoints[1]]} points`);
+    console.log(`3rd Place: ${sortedpoints[2]} with ${point[sortedpoints[2]]} points`);
+}
+
+OpeningCeremony({ red: 0, blue: 0, green: 0, yellow: 0 }, Race100M);
